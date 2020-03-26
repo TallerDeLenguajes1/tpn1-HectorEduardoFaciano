12d78667b0153664b6a29ad2e758007852d92af0
HectorEduardoFaciano <62724370+HectorEduardoFaciano@users.noreply.github.com>
2020-03-26 19:51:02 -0300

    Update tp1_1.cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(){
+    int x = 1, *punt;
+    punt = &x;
+
+    printf("Contenido del puntero: %d\n", *punt);
+    printf("Direccion de memoria del puntero: %d\n", punt);
+    printf("Direccion de la variable: %d\n", &x);
+    printf("Direccion de memoria del puntero: %d\n", &punt); 
+   	printf("Tamaño de memoria utilizado por esa variable: %d", sizeof(x));
+
+    return 0;
+}
